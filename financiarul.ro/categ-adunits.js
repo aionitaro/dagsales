0cf2335102f3a20e20add75a2acb62cb19b05f79
zgomotos <gogu.samsung@gmail.com>
2021-03-12 22:09:14 +0200

    category updated

---
+++ financiarul.ro/categ-adunits.js
@@ -0,0 +1,253 @@
+var adUnits = [
+    {
+        code: 'billboard',
+        mediaTypes: {
+            banner: {
+                sizeConfig: [
+                    { minViewPort: [0, 0], sizes: [[300, 250]] },
+                    { minViewPort: [980, 0], sizes: [[970,250],[728,90]] }
+                ]
+            }
+        },
+        bids: [
+            {bidder: 'adform', params: { mid: 998849} },
+            {bidder: "rtbhouse",params: {region: 'prebid-eu',publisherId: 'FxXwYu16CSnfjgxF0NQH'}},
+            {"bidder":"criteo","params":{"networkId":8796}}
+           
+        ]
+    },
+    {
+        code: 'inread',
+        mediaTypes: {
+            banner: {
+                sizeConfig: [
+                    { minViewPort: [0, 0], sizes: [[300, 250]] },
+                    { minViewPort: [980, 0], sizes: [[480,320], [336,280], [300,250]] }
+                ]
+            }
+        },
+        bids: [
+            {bidder: 'adform', params: { mid: 998848} },
+            {bidder: "rtbhouse",params: {region: 'prebid-eu',publisherId: 'FxXwYu16CSnfjgxF0NQH'}},
+            {"bidder":"criteo","params":{"networkId":8796}}
+
+        ]
+    },
+    {
+        code: 'inread2',
+        mediaTypes: {
+            banner: {
+                sizeConfig: [
+                    { minViewPort: [0, 0], sizes: [[300, 250]] },
+                    { minViewPort: [980, 0], sizes: [[480,320], [336,280], [300,250]] }
+                ]
+            }
+        },
+        bids: [
+            {bidder: 'adform', params: { mid: 998848} },
+            {bidder: "rtbhouse",params: {region: 'prebid-eu',publisherId: 'FxXwYu16CSnfjgxF0NQH'}},
+            {"bidder":"criteo","params":{"networkId":8796}}
+
+        ]
+    },
+    {
+        code: 'inread3',
+        mediaTypes: {
+            banner: {
+                sizeConfig: [
+                    { minViewPort: [0, 0], sizes: [[300, 250]] },
+                    { minViewPort: [980, 0], sizes: [[480,320], [336,280], [300,250]] }
+                ]
+            }
+        },
+        bids: [
+            {bidder: 'adform', params: { mid: 998848} },
+            {bidder: "rtbhouse",params: {region: 'prebid-eu',publisherId: 'FxXwYu16CSnfjgxF0NQH'}},
+            {"bidder":"criteo","params":{"networkId":8796}}
+
+        ]
+    },
+    {
+        code: 'inread4',
+        mediaTypes: {
+            banner: {
+                sizeConfig: [
+                    { minViewPort: [0, 0], sizes: [[300, 250]] },
+                    { minViewPort: [980, 0], sizes: [[480,320], [336,280], [300,250]] }
+                ]
+            }
+        },
+        bids: [
+            {bidder: 'adform', params: { mid: 998848} },
+            {bidder: "rtbhouse",params: {region: 'prebid-eu',publisherId: 'FxXwYu16CSnfjgxF0NQH'}},
+            {"bidder":"criteo","params":{"networkId":8796}}
+
+        ]
+    },
+    {
+        code: 'inread5',
+        mediaTypes: {
+            banner: {
+                sizeConfig: [
+                    { minViewPort: [0, 0], sizes: [[300, 250]] },
+                    { minViewPort: [980, 0], sizes: [[480,320], [336,280], [300,250]] }
+                ]
+            }
+        },
+        bids: [
+            {bidder: 'adform', params: { mid: 998848} },
+            {bidder: "rtbhouse",params: {region: 'prebid-eu',publisherId: 'FxXwYu16CSnfjgxF0NQH'}},
+            {"bidder":"criteo","params":{"networkId":8796}}
+
+        ]
+    },
+    {
+        code: 'inread6',
+        mediaTypes: {
+            banner: {
+                sizeConfig: [
+                    { minViewPort: [0, 0], sizes: [[300, 250]] },
+                    { minViewPort: [980, 0], sizes: [[480,320], [336,280], [300,250]] }
+                ]
+            }
+        },
+        bids: [
+            {bidder: 'adform', params: { mid: 998848} },
+            {bidder: "rtbhouse",params: {region: 'prebid-eu',publisherId: 'FxXwYu16CSnfjgxF0NQH'}},
+            {"bidder":"criteo","params":{"networkId":8796}}
+
+        ]
+    },
+    {
+        code: 'inread7',
+        mediaTypes: {
+            banner: {
+                sizeConfig: [
+                    { minViewPort: [0, 0], sizes: [[300, 250]] },
+                    { minViewPort: [980, 0], sizes: [[480,320], [336,280], [300,250]] }
+                ]
+            }
+        },
+        bids: [
+            {bidder: 'adform', params: { mid: 998848} },
+            {bidder: "rtbhouse",params: {region: 'prebid-eu',publisherId: 'FxXwYu16CSnfjgxF0NQH'}},
+            {"bidder":"criteo","params":{"networkId":8796}}
+
+        ]
+    },
+    {
+        code: 'inread8',
+        mediaTypes: {
+            banner: {
+                sizeConfig: [
+                    { minViewPort: [0, 0], sizes: [[300, 250]] },
+                    { minViewPort: [980, 0], sizes: [[480,320], [336,280], [300,250]] }
+                ]
+            }
+        },
+        bids: [
+            {bidder: 'adform', params: { mid: 998848} },
+            {bidder: "rtbhouse",params: {region: 'prebid-eu',publisherId: 'FxXwYu16CSnfjgxF0NQH'}},
+            {"bidder":"criteo","params":{"networkId":8796}}
+
+        ]
+    },
+    {
+        code: 'inread9',
+        mediaTypes: {
+            banner: {
+                sizeConfig: [
+                    { minViewPort: [0, 0], sizes: [[300, 250]] },
+                    { minViewPort: [980, 0], sizes: [[480,320], [336,280], [300,250]] }
+                ]
+            }
+        },
+        bids: [
+            {bidder: 'adform', params: { mid: 998848} },
+            {bidder: "rtbhouse",params: {region: 'prebid-eu',publisherId: 'FxXwYu16CSnfjgxF0NQH'}},
+            {"bidder":"criteo","params":{"networkId":8796}}
+
+        ]
+    },
+    {
+        code: 'sidebar',
+        mediaTypes: {
+            banner: {
+                sizeConfig: [
+                    { minViewPort: [0, 0], sizes: [[300, 250]] }
+                ]
+            }
+        },
+        bids: [
+            {bidder: 'adform', params: { mid: 998848} },
+            {bidder: "rtbhouse",params: {region: 'prebid-eu',publisherId: 'FxXwYu16CSnfjgxF0NQH'}},
+            {"bidder":"criteo","params":{"networkId":8796}}
+
+        ]
+    },
+    {
+        code: 'sidebar1',
+        mediaTypes: {
+            banner: {
+                sizeConfig: [
+                    { minViewPort: [0, 0], sizes: [[300, 250]] }
+                ]
+            }
+        },
+        bids: [
+             {bidder: 'adform', params: { mid: 998848} },
+            {bidder: "rtbhouse",params: {region: 'prebid-eu',publisherId: 'FxXwYu16CSnfjgxF0NQH'}},
+            {"bidder":"criteo","params":{"networkId":8796}}
+
+        ]
+    },
+    {
+        code: 'sidebar2',
+        mediaTypes: {
+            banner: {
+                sizeConfig: [
+                    { minViewPort: [0, 0], sizes: [[300, 250]] }
+                ]
+            }
+        },
+        bids: [
+             {bidder: 'adform', params: { mid: 998848} },
+            {bidder: "rtbhouse",params: {region: 'prebid-eu',publisherId: 'FxXwYu16CSnfjgxF0NQH'}},
+            {"bidder":"criteo","params":{"networkId":8796}}
+
+        ]
+    },
+    {
+        code: 'sidebar3',
+        mediaTypes: {
+            banner: {
+                sizeConfig: [
+                    { minViewPort: [0, 0], sizes: [[300, 250]] }
+                ]
+            }
+        },
+        bids: [
+        {bidder: 'adform', params: { mid: 998848} },
+            {bidder: "rtbhouse",params: {region: 'prebid-eu',publisherId: 'FxXwYu16CSnfjgxF0NQH'}},
+            {"bidder":"criteo","params":{"networkId":8796}}
+
+        ]
+    },
+    {
+        code: 'sidebar4',
+        mediaTypes: {
+            banner: {
+                sizeConfig: [
+                    { minViewPort: [0, 0], sizes: [[300, 250]] }
+                ]
+            }
+        },
+        bids: [
+            {bidder: 'adform', params: { mid: 998848} },
+            {bidder: "rtbhouse",params: {region: 'prebid-eu',publisherId: 'FxXwYu16CSnfjgxF0NQH'}},
+            {"bidder":"criteo","params":{"networkId":8796}}
+
+        ]
+    },
+   
+    ];
